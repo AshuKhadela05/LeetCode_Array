540bbd7bf7ecebeb4c5db6537784986b551687eb
Ashish Khadela <jupg24msc20378@jainuniversity.ac.in>
2025-04-14 10:26:52 +0530

    MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -4,43 +4,44 @@ public class MajorityElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
+        // Step 1: Input size of the array
         System.out.println("Enter number of elements:");
         int n = sc.nextInt();
 
         int[] nums = new int[n];
 
-
+        // Step 2: Input array elements
         System.out.println("Enter the elements:");
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
 
-
+        // Step 3: Call the majorityElement method using Solution class
         Solution4 solution = new Solution4();
         int result = solution.majorityElement(nums);
 
-
+        // Step 4: Print the majority element
         System.out.println("Majority Element: " + result);
 
-        sc.close();
+        sc.close();    // Close scanner 
     }
 }
 
+// This class contains the logic to find the majority element using Boyer-Moore Voting Algorithm
 class Solution4 {
     public int majorityElement(int[] nums) {
-        int majority = 0;
-        int votes= 0;
+        int majority = 0;     // Candidate for majority element
+        int votes= 0;         // Vote counter
 
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
 
-
+            // If no current candidate, pick the current number
             if (votes == 0) {
                 majority = num;
             }
 
-
+            // If current number matches candidate, increment votes
             if (num == majority) {
                 votes = votes + 1;
             } else {
@@ -48,7 +49,7 @@ class Solution4 {
             }
         }
 
-       
+        // Return the majority element
         return majority;
     }
 }
